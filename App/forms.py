9a5f67158d3844882b121535307d9d365c7e329c
GARG <garg@GARGs-MacBook-Air.local>
2020-05-29 19:02:59 +0530

    Web App Updated

--- App/forms.py
+++ App/forms.py
@@ -3,6 +3,10 @@ from wtforms import StringField, SubmitField, TextField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 class MovieInputForm(FlaskForm):
-    moviename = StringField('Movie Name', validators=[DataRequired()])
+    moviename = TextField('Movie Name', validators=[DataRequired()])
     moviereview = TextAreaField('Movie Review', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class SearchForm(FlaskForm):
+    moviename = TextField('Movie Name', validators=[DataRequired()])
     submit = SubmitField('Submit')
